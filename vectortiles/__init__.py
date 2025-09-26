4561d530ad3c83ed6efa1e3c2254fdc2b96bdb68
Steve Pepple <stephen.pepple@gmail.com>
2025-09-26 15:28:54 -0700

    Up with latest from working project

--- vectortiles/__init__.py
+++
@@ -1,5 +0,0 @@
-from django.utils.module_loading import import_string
-
-from vectortiles import settings as app_settings
-
-VectorLayer = import_string(f"{app_settings.VECTOR_TILES_BACKEND}.VectorLayer")
--- vectortiles/mixins.py
+++ vectortiles/mixins.py
@@ -1,182 +1,135 @@
-from urllib.parse import unquote, urljoin
+from urllib.parse import unquote
 
-from django.urls import path
-
-from vectortiles import settings as app_settings
-
-
-class BaseVectorView:
-    layer_classes = None
-    layers = None
-    prefix_tiles_url = "tiles"
-
-    def get_layer_classes(self):
-        return self.layer_classes or []
-
-    def get_layer_class_kwargs(self):
-        return {}
-
-    def get_layers(self):
-        return (
-            self.layers
-            if self.layers
-            else [
-                layer_class(**self.get_layer_class_kwargs())
-                for layer_class in self.get_layer_classes()
-            ]
-        )
+import mercantile
+from django.http import HttpResponse
 
 
-class BaseTileJSONView(BaseVectorView):
-    # https://github.com/mapbox/tilejson-spec/tree/master/3.0.0
-    name = None
-    attribution = None
-    description = None
-    min_zoom = None  # 0 - 30 range. lower than equal than max_zoom. By default, it's min_zoom of all layers.
-    max_zoom = None  # 0 - 30 range. greater than equal than min_zoom. By default, it's max_zoom of all layers.
-    fill_zoom = None
-    legend = None
-    bounds = [-180, -85.05112877980659, 180, 85.0511287798066]
-    center = None
-    scheme = "xyz"
-    version = "1.0.0"
+class BaseVectorTileMixin:
+    vector_tile_content_type = "application/x-protobuf"
+    vector_tile_queryset = None
+    vector_tile_layer_name = None  # name for data layer in vector tile
+    vector_tile_geom_name = "geom"  # geom field to consider in qs
+    vector_tile_fields = None  # other fields to include from qs
+    vector_tile_generation = None  # use mapbox if you installed [mapbox] subdependencies
+    vector_tile_extent = 4096
+    vector_tile_buffer = 256
 
-    def __init__(self, *args, **kwargs):
-        if self.get_min_zoom() > self.get_max_zoom():
-            raise ValueError("min_zoom must be lower than equal than max_zoom")
-        if self.get_max_zoom() < self.get_min_zoom():
-            raise ValueError("max_zoom must be greater than equal than min_zoom")
-        if not (0 <= self.get_min_zoom() <= 30):
-            raise ValueError("min_zoom should be in range 0 - 30")
-        if not (0 <= self.get_max_zoom() <= 30):
-            raise ValueError("max_zoom should be in range 0 - 30")
-        super().__init__(*args, **kwargs)
+    def get_bounds(self, x, y, z):
+        xmin, ymin, xmax, ymax = mercantile.xy_bounds(x, y, z)
+        return xmin, ymin, xmax, ymax
 
-    def get_min_zoom(self):
-        """Get tilejson minzoom from layers or self.min_zoom"""
-        try:
-            # minimum zoom level from layers
-            layers_min_zoom = min(layer.get_min_zoom() for layer in self.get_layers())
-        except ValueError:
-            # case there is no layer defined ...
-            layers_min_zoom = None
+    def get_vector_tile_queryset(self):
+        return self.vector_tile_queryset if self.vector_tile_queryset else self.get_queryset()
 
-        if self.min_zoom is not None:
-            # if defined, return max of layers_min_zoom and self.min_zoom
-            return (
-                max(layers_min_zoom, self.min_zoom)
-                if layers_min_zoom is not None
-                else self.min_zoom
-            )
-        # if self.min_zoom not defined, return layers_min_zoom or 0
-        return layers_min_zoom if layers_min_zoom is not None else 0
+    def get_vector_tile_layer_name(self):
+        return self.vector_tile_layer_name
 
-    def get_max_zoom(self):
-        """Get tilejson maxzoom from layers or self.max_zoom"""
-        try:
-            # maximum zoom level from layers
-            layers_max_zoom = min(layer.get_max_zoom() for layer in self.get_layers())
-        except ValueError:
-            # case there is no layer defined ...
-            layers_max_zoom = None
+    def get_tile(self, x, y, z, extent=4096, buffer=256, clip_geom=True):
+        """
+        Get bytearray representing a MapBoxVectorTile
+        :param x: longitude coordinate tile
+        :param y: latitude coordinate tile
+        :param z: zoom level
+        :param extent: tile extent
+        :param buffer: buffer extent
+        :param clip_geom: clip geometries
+        :param geom_field: Geometry field name in subquery
+        :param include_fields: extra fields to include in Vector tile
+        :return: mvt as bytearray
+        """
+        raise NotImplementedError()
 
-        if self.max_zoom is not None:
-            # if defined, return min of layers_max_zoom and self.max_zoom
-            return (
-                min(layers_max_zoom, self.max_zoom)
-                if layers_max_zoom is not None
-                else self.max_zoom
-            )
-        # if self.max_zoom not defined, return layers_max_zoom or 30
-        return layers_max_zoom if layers_max_zoom is not None else 30
 
-    def get_fill_zoom(self):
-        return self.fill_zoom
+class VectorLayer:
+    vector_tile_layer_id = ""
+    vector_tile_layer_description = ""
+    vector_tile_layer_min_zoom = 0
+    vector_tile_layer_max_zoom = 22
 
-    def get_attribution(self):
-        return self.attribution
+    def __init__(self, id_layer, description="", min_zoom=0, max_zoom=22):
+        self.vector_tile_layer_id = id_layer
+        self.vector_tile_layer_description = description
+        self.vector_tile_layer_min_zoom = min_zoom
+        self.vector_tile_layer_max_zoom = max_zoom
 
-    def get_legend(self):
-        return self.legend
+    def get_vector_tile_layer_id(self):
+        return self.vector_tile_layer_id
 
-    def get_description(self):
-        return self.description
+    def get_vector_tile_layer_description(self):
+        return self.vector_tile_layer_description
 
-    def get_name(self):
-        return self.name
+    def get_vector_tile_layer_min_zoom(self):
+        return self.vector_tile_layer_min_zoom
 
-    def get_bounds(self):
-        return self.bounds
+    def get_vector_tile_layer_max_zoom(self):
+        return self.vector_tile_layer_max_zoom
 
-    def get_center(self):
-        return self.center
-
-    def get_scheme(self):
-        return self.scheme
-
-    def get_version(self):
-        return self.version
-
-    def get_tile_urls(self, tile_url):
-        if app_settings.VECTOR_TILES_URLS:
-            return [
-                unquote(urljoin(hostname, tile_url))
-                for hostname in app_settings.VECTOR_TILES_URLS
-            ]
-        else:
-            return [unquote(self.request.build_absolute_uri(tile_url))]
-
-    def get_tilejson(self, tile_url, version="3.0.0"):
-        # https://github.com/mapbox/tilejson-spec/tree/master/3.0.0
+    def get_vector_layer(self):
         return {
-            "tilejson": version,
-            "name": self.get_name(),
-            "description": self.get_description(),
-            "legend": self.get_legend(),
-            "attribution": self.get_attribution(),
-            "tiles": self.get_tile_urls(tile_url),
-            "minzoom": self.get_min_zoom(),
-            "maxzoom": self.get_max_zoom(),
-            "fillzoom": self.get_fill_zoom(),
-            "bounds": self.get_bounds(),
-            "center": self.get_center(),
-            "scheme": self.get_scheme(),
-            "version": self.get_version(),
-            "vector_layers": [
-                layer.get_tilejson_vector_layer() for layer in self.get_layers()
-            ],
+            'id': self.get_vector_tile_layer_id(),
+            'description': self.get_vector_tile_layer_description(),
+            # 'fields': self.layer_fields(layer),
+            'minzoom': self.get_vector_tile_layer_min_zoom(),
+            'maxzoom': self.get_vector_tile_layer_max_zoom(),
         }
 
 
-class BaseVectorTileView(BaseVectorView):
-    """Base mixin to handle vector tile in a django View"""
+class BaseTileJSONMixin:
+    vector_tile_tilejson_name = ""
+    vector_tile_tilejson_attribution = ""
+    vector_tile_tilejson_description = ""
 
-    content_type = app_settings.VECTOR_TILES_CONTENT_TYPE
+    def get_vector_tile_tilejson_min_zoom(self):
+        min_zoom = min(item['minzoom'] for item in self.get_vector_layers())
+        return min_zoom or 0
 
-    def get_layer_tiles(self, z, x, y):
-        layers = self.get_layers()
-        if layers:
-            return b"".join(layer.get_tile(x, y, z) for layer in layers)
-        raise Exception("No layers defined")
+    def get_vector_tile_tilejson_max_zoom(self):
+        max_zoom = max(item['maxzoom'] for item in self.get_vector_layers())
+        return max_zoom or 22
 
-    def get_content_status(self, z, x, y):
-        content = self.get_layer_tiles(z, x, y)
-        return (content, 200) if content else (content, 204)
+    def get_vector_tile_tilejson_attribution(self):
+        return self.vector_tile_tilejson_attribution
 
-    def get_base_url(self):
-        pass
+    def get_vector_tile_tilejson_description(self):
+        return self.vector_tile_tilejson_description
 
-    def get_default_url_pattern(self):
-        return "{z}/{x}/{y}"
+    def get_vector_tile_tilejson_name(self):
+        return self.vector_tile_tilejson_name
 
-    def get_default_url_matrix(self):
-        pattern = self.get_default_url_pattern()
-        return f"{pattern.replace('{z}', '<int:z>').replace('{x}', '<int:x>').replace('{y}', '<int:y>')}"
+    def get_tile_urls(self, tile_url, base_url=''):
+        # if app_settings.TERRA_TILES_HOSTNAMES:
+        #     return [
+        #         unquote(urljoin(hostname, tile_url))
+        #         for hostname in app_settings.VECTOR_TILES_HOSTNAMES
+        #     ]
+        # else:
+        return [
+            unquote(tile_url)
+        ]
 
-    def get_url(self, prefix=None, url_name=None):
-        """Generate URL to serve vector tiles with required parameters"""
-        return path(
-            f"{prefix or self.prefix_tiles_url}/{self.get_default_url_matrix()}",
-            self.as_view(),
-            name=url_name,
-        )
+    def get_vector_layers(self):
+        raise NotImplementedError(""" you should implement get_vector_layers to return a VectorLayer list """)
+
+    def get_tilejson(self, tile_url, version="3.0.0"):
+        # https://github.com/mapbox/tilejson-spec/tree/3.0/3.0.0
+        return {
+            'tilejson': version,
+            'name': self.get_vector_tile_tilejson_name(),
+            'tiles': self.get_tile_urls(tile_url),
+            'minzoom': self.get_vector_tile_tilejson_min_zoom(),
+            'maxzoom': self.get_vector_tile_tilejson_max_zoom(),
+            # bounds
+            # center
+            'attribution': self.get_vector_tile_tilejson_attribution(),
+            'description': self.get_vector_tile_tilejson_description(),
+            'vector_layers': self.get_vector_layers(),
+        }
+
+
+class BaseVectorTileView:
+    content_type = "application/vnd.mapbox-vector-tile"
+
+    def get(self, request, z, x, y):
+        content = self.get_tile(x, y, z, extent=self.vector_tile_extent, buffer=self.vector_tile_buffer, clip_geom=True)
+        status = 200 if content else 204
+        return HttpResponse(content, content_type=self.content_type, status=status)
